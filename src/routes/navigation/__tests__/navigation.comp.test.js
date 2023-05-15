9be704659c1e5147cb5fa84fe27c99852e3ede2b
Danielxburns <danielburnstech@gmail.com>
2023-05-15 13:37:54 -0500

    finish testing navigation component

--- src/routes/navigation/__tests__/navigation.comp.test.js
+++ src/routes/navigation/__tests__/navigation.comp.test.js
@@ -1,36 +1,60 @@
-import { screen } from '@testing-library/react'
-import Navigation from '../navigation.component'
-import { renderWithProviders } from '../../../assets/utils/test/test.utils'
-import exp from 'constants'
-import { sign } from 'crypto'
+import { screen } from '@testing-library/react';
+import Navigation from '../navigation.component';
+import { renderWithProviders } from '../../../assets/utils/test/test.utils';
 
 describe('Navigation tests', () => {
   test('It should render a Sign In link and not a Sign Out link if there is no current user', () => {
-    renderWithProviders(<Navigation/>, {
+    renderWithProviders(<Navigation />, {
       preLoadedState: {
         user: {
-          currentUser: null
-        }
-      }
+          currentUser: null,
+        },
+      },
     });
     const signOutLinkElement = screen.queryByText(/sign out/i);
     expect(signOutLinkElement).toBeNull();
 
     const signInLinkElement = screen.getByText(/sign in/i);
     expect(signInLinkElement).toBeInTheDocument();
-  })
-  test("it should render Sign Out and not Sign In if there is a current user", () => {
-    renderWithProviders(<Navigation/>, {
+  });
+  test('it should render Sign Out and not Sign In if there is a current user', () => {
+    renderWithProviders(<Navigation></Navigation>, {
       preLoadedState: {
         user: {
-          currentUser: 'user'
-        }
-      }
+          currentUser: 'user',
+        },
+      },
     });
     const signInLinkElement = screen.queryByText(/sign in/i);
     expect(signInLinkElement).toBeNull();
 
     const signOutLinkElement = screen.getByText(/sign out/i);
     expect(signOutLinkElement).toBeInTheDocument();
-  })
-})
+  });
+
+  test('it should not render a cart dropdown if cartIsOpen is false', () => {
+    renderWithProviders(<Navigation />, {
+      preLoadedState: {
+        cart: {
+          isCartOpen: false,
+          cartItems: [],
+        },
+      },
+    });
+    const cartDropdownTextElement = screen.queryByText(/your cart is empty/i);
+    expect(cartDropdownTextElement).toBeNull();
+  });
+
+  test('it should render a cart dropdown if cartIsOpen is true', () => {
+    renderWithProviders(<Navigation />, {
+      preLoadedState: {
+        cart: {
+          isCartOpen: true,
+          cartItems: [],
+        },
+      },
+    });
+    const cartDropdownTextElement = screen.getByText(/your cart is empty/i);
+    expect(cartDropdownTextElement).toBeInTheDocument();
+  });
+});
