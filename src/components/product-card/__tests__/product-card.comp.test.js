e1a6ea03e00546476fecc2bca0009c1cadbd3438
Danielxburns <danielburnstech@gmail.com>
2023-05-15 18:28:11 -0500

    add product card tests

---
+++ src/components/product-card/__tests__/product-card.comp.test.js
@@ -0,0 +1,26 @@
+import { screen, fireEvent } from "@testing-library/react";
+import { renderWithProviders } from "../../../assets/utils/test/test.utils";
+import ProductCard from "../product-card.component";
+
+describe("Product Card tests", () => {
+  test('it should add the product item when product card button is clicked', async () => {
+    const mockProduct = {
+      id: 1,
+      imageUrl: 'test',
+      name: 'item1',
+      price: 10,
+    }
+    
+    const { store } = renderWithProviders(<ProductCard product={mockProduct}/>, {
+      preLoadedState: {
+        cart: {
+          cartItems: []
+        }
+      }
+    });
+    const addToCartButtonElement = screen.getByText(/add to cart/i);
+    await fireEvent.click(addToCartButtonElement);
+    console.log('cartItems :>> ', store.getState().cart.cartItems);
+    expect(store.getState().cart.cartItems.length).toBe(1)
+  })
+})
