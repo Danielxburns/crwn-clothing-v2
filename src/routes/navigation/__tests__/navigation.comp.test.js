5b3616f94edc6f94ddefd2bfd87a9d2260e2df3a
Danielxburns <danielburnstech@gmail.com>
2023-05-16 12:33:06 -0500

    add test to check that dispatch is fired with sign out action

--- src/routes/navigation/__tests__/navigation.comp.test.js
+++ src/routes/navigation/__tests__/navigation.comp.test.js
@@ -1,8 +1,28 @@
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
+import * as reactRedux from 'react-redux';
 import Navigation from '../navigation.component';
 import { renderWithProviders } from '../../../assets/utils/test/test.utils';
+import { signOutStart } from '../../../store/user/user.action';
 
 describe('Navigation tests', () => {
+  test('It should dispatch signOutStart action when clicking on the Sign Out link', async () => {
+    const mockDispatch = jest.fn();
+    jest.spyOn(reactRedux, "useDispatch").mockReturnValue(mockDispatch);
+    renderWithProviders(<Navigation />, {
+      preLoadedState: {
+        user: {
+          currentUser: {}
+        }
+      }
+    })
+    const signOutLinkelement = screen.getByText(/sign out/i);
+    expect(signOutLinkelement).toBeInTheDocument();
+    await fireEvent.click(signOutLinkelement);
+    expect(mockDispatch).toHaveBeenCalled();
+    expect(mockDispatch).toHaveBeenCalledWith(signOutStart());
+    mockDispatch.mockClear();
+  })
+
   test('It should render a Sign In link and not a Sign Out link if there is no current user', () => {
     renderWithProviders(<Navigation />, {
       preLoadedState: {
@@ -21,7 +41,7 @@ describe('Navigation tests', () => {
     renderWithProviders(<Navigation></Navigation>, {
       preLoadedState: {
         user: {
-          currentUser: 'user',
+          currentUser: {},
         },
       },
     });
